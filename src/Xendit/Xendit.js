f01b382c92138ac0701ef86974984743e5f1fd8f
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-12-01 14:29:56 +0700

    remove xendit

--- src/Xendit/Xendit.js
+++
@@ -1,8 +0,0 @@
-// xendit.js
-const config = {
-    publishableKey: 'xnd_public_development_v02wPa5o4Jo4NlaRzgcDJ8pqAn9IrP8px3AjwmE2n45zGPiEFGCr3jc283Y06HD',
-    secretKey: 'xnd_development_iXq67NTTRmQghVf3Se39PCwGSZq3oI29UAMH5nMhmrXqhLX5J4a07kKCke52MA5',
-  };
-  
-  export default config;
-  
