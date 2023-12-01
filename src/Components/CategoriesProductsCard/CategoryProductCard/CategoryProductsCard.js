851e9c6684537c7853fe8605c24b29b867008778
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-12-01 14:36:40 +0700

    fix: add to cart redux function user

--- src/Components/CategoriesProductsCard/CategoryProductCard/CategoryProductsCard.js
+++ src/Components/CategoriesProductsCard/CategoryProductCard/CategoryProductsCard.js
@@ -35,11 +35,10 @@ const CategoryProductsCard = ({ products }) => {
   const handleClose = () => setOpen(false);
   
   // Get user authentication status from Redux state
-  const user = useSelector(state => state.user);
+  const user = useSelector(state => state.userAuth);
 
   useEffect(() => {
     let isMounted = true;
-
     if (isActive) {
       setTimeout(() => {
         if (isMounted) {
@@ -58,6 +57,7 @@ const CategoryProductsCard = ({ products }) => {
   };
 
   const onClickHandler = () => {
+    console.log(user)
     if (!user) {
       // Redirect to login page if user is not authenticated
       history.push('/login');
--- src/Redux/userAuth/userAuth_reducers.js
+++ src/Redux/userAuth/userAuth_reducers.js
@@ -1,7 +1,7 @@
 const userReducer = (state = null, action) => {
   switch (action.type) {
     case 'LOGGED_IN_USER':
-      return action.payload;
+      return { ...action.payload };
 
     case 'LOGOUT':
       return action.payload;
