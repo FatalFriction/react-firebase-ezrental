d0e8d715a50910d60ca0b6e354052a50d234ca2b
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-12-01 14:30:02 +0700

    remove speechly

--- src/SpeechlyAi/config.js
+++ src/SpeechlyAi/config.js
@@ -1,6 +1,5 @@
 import { createSpeechlySpeechRecognition } from '@speechly/speech-recognition-polyfill';
 import SpeechRecognition from 'react-speech-recognition';
 
-const appId = '278783f8-cbd5-4068-a370-8bbda3f911f1';
-const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(process.env.SPEECHLY_ID);
 SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
